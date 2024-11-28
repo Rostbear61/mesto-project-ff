db2e2ff20259e11841758b064c444f97ebea5144
Dmitry Loshakov <loshakovdmitry@ya.ru>
2024-11-28 11:43:29 +0300

    second try_2

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,18 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardConteiner = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCard(img, title){
+function createCard(item, {deleteCard}){
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImg = cardElement.querySelector('img');
-    cardImg.src = img;
-    cardImg.alt = 'Вид на ' + title;
-    cardElement.querySelector('.card__title').textContent = title;
+    cardImg.src = item.link;
+    cardImg.alt = 'Вид на ' + item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
     
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', function(evt){
         removeCard(evt.target.closest('.card'));
     });
+
     return cardElement;
 }
 // @todo: Функция удаления карточки
@@ -25,5 +26,5 @@ function addCard(cardElement){
     cardConteiner.append(cardElement);
 }
 for (let i = 0; i < 6; i++){
-    addCard(createCard(initialCards[i].link, initialCards[i].name));
+    addCard(createCard(initialCards[i], removeCard));
 }
