33607bc475fa88659e9c93ebca3d47b55310026c
Dmitry Loshakov <loshakovdmitry@ya.ru>
2024-11-28 08:51:24 +0300

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -3,24 +3,27 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardConteiner = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(img, title){
+function createCard(img, title){
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('img').src = img;
+    const cardImg = cardElement.querySelector('img');
+    cardImg.src = img;
+    cardImg.alt = 'Вид на ' + title;
     cardElement.querySelector('.card__title').textContent = title;
-    cardConteiner.append(cardElement);
-
+    
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', function(evt){
-    removeCard(evt.target.parentElement);
+        removeCard(evt.target.closest('.card'));
     });
+    return cardElement;
 }
 // @todo: Функция удаления карточки
 function removeCard(element){
     element.remove();
 }
 // @todo: Вывести карточки на страницу
-for (let i = 0; i < 6; i++){
-    addCard(initialCards[i].link, initialCards[i].name);
+function addCard(cardElement){
+    cardConteiner.append(cardElement);
 }
-
-
+for (let i = 0; i < 6; i++){
+    addCard(createCard(initialCards[i].link, initialCards[i].name));
+}
